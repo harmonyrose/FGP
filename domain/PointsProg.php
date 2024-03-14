b5e5c00b30e514ea3afd6c554dc8115945cedfcd
hpeura <hpeura@mail.umw.edu>
2024-03-13 22:25:33 -0400

    Added Points prog class

--- domain/PointsProg.php
+++ domain/PointsProg.php
@@ -5,78 +5,139 @@
 class PointsProg {
     private $id;
     private $name;
-    private $abbrevName;
-    private $date;
-    private $startTime;
-    private $endTime;
-    private $description;
-    private $location;
-    private $capacity;
-    private $trainingMedia;
-    private $postMedia;
-    private $animalId;
+    private $address;
+    private $freezer_meals;
+    private $allergies;
+    private $snacks;
+    private $snack_notes;
+    private $foodlion;
+    private $giant;
+    private $walmart;
+    private $wegmans;
+    private $sheetz;
+    private $wawa;
+    private $house_cleaning;
+    private $lawn_care;
+    private $AAA_membership;
+    private $AAA_membership_name;
+    private $AAA_membership_DOB;
+    private $photography;
+    private $house_projects;
+    private $financial_relief;
+    private $points_used;
 
-    function __construct($id, $name, $abbrevName, $date, $startTime, $endTime, $description, $location, $capacity, $trainingMedia, $postMedia, $animalId) {
+    // Constructor
+    public function __construct($id, $name, $address, $freezer_meals, $allergies, $snacks, $snack_notes, $foodlion, $giant, $walmart, $wegmans, $sheetz, $wawa, $house_cleaning, $lawn_care, $AAA_membership, $AAA_membership_name, $AAA_membership_DOB, $photography, $house_projects, $financial_relief, $points_used) {
         $this->id = $id;
         $this->name = $name;
-        $this->abbrevName = $abbrevName;
-        $this->date = $date;
-        $this->startTime = $startTime;
-        $this->endTime = $endTime;
-        $this->description = $description;
-        $this->location = $location;
-        $this->capacity = $capacity;
-        $this->trainingMedia = $trainingMedia;
-        $this->postMedia = $postMedia;
-        $this->animalId = $animalId;
+        $this->address = $address;
+        $this->freezer_meals = $freezer_meals;
+        $this->allergies = $allergies;
+        $this->snacks = $snacks;
+        $this->snack_notes = $snack_notes;
+        $this->foodlion = $foodlion;
+        $this->giant = $giant;
+        $this->walmart = $walmart;
+        $this->wegmans = $wegmans;
+        $this->sheetz = $sheetz;
+        $this->wawa = $wawa;
+        $this->house_cleaning = $house_cleaning;
+        $this->lawn_care = $lawn_care;
+        $this->AAA_membership = $AAA_membership;
+        $this->AAA_membership_name = $AAA_membership_name;
+        $this->AAA_membership_DOB = $AAA_membership_DOB;
+        $this->photography = $photography;
+        $this->house_projects = $house_projects;
+        $this->financial_relief = $financial_relief;
+        $this->points_used = $points_used;
     }
 
-    function getID() {
+    // Getters
+    public function getId() {
         return $this->id;
     }
 
-    function getName() {
+    public function getName() {
         return $this->name;
     }
 
-    function getAbbreviatedName() {
-        return $this->abbrevName;
+    public function getAddress() {
+        return $this->address;
     }
 
-    function getDate() {
-        return $this->date;
+    public function getFreezerMeals() {
+        return $this->freezer_meals;
     }
 
-    function getStartTime() {
-        return $this->startTime;
+    public function getAllergies() {
+        return $this->allergies;
     }
 
-    function getEndTime() {
-        return $this->endTime;
+    public function getSnacks() {
+        return $this->snacks;
     }
 
-    function getDescription() {
-        return $this->description;
+    public function getSnackNotes() {
+        return $this->snack_notes;
     }
 
-    function getLocation() {
-        return $this->location;
+    public function getFoodlion() {
+        return $this->foodlion;
     }
 
-    function getCapacity() {
-        return $this->capacity;
+    public function getGiant() {
+        return $this->giant;
     }
 
-
-    function getTrainingMedia() {
-        return $trainingMedia;
+    public function getWalmart() {
+        return $this->walmart;
     }
 
-    function getPostMedia() {
-        return $postMedia;
+    public function getWegmans() {
+        return $this->wegmans;
     }
 
-    function getAnimalId() {
-        return $animalId;
+    public function getSheetz() {
+        return $this->sheetz;
+    }
+
+    public function getWawa() {
+        return $this->wawa;
+    }
+
+    public function getHouseCleaning() {
+        return $this->house_cleaning;
+    }
+
+    public function getLawnCare() {
+        return $this->lawn_care;
+    }
+
+    public function getAAAMembership() {
+        return $this->AAA_membership;
+    }
+
+    public function getAAAMembershipName() {
+        return $this->AAA_membership_name;
+    }
+
+    public function getAAAMembershipDOB() {
+        return $this->AAA_membership_DOB;
+    }
+
+    public function getPhotography() {
+        return $this->photography;
+    }
+
+    public function getHouseProjects() {
+        return $this->house_projects;
+    }
+
+    public function getFinancialRelief() {
+        return $this->financial_relief;
+    }
+
+    public function getPointsUsed() {
+        return $this->points_used;
     }
 }
