8d8bb905f0ca77e23a3c4bc950e7c88b95a49cf9
Erk4Code <ebae1701@gmail.com>
2024-04-02 00:31:35 -0400

    Changed the in built system for adding volunteers. Cleaned the code and uses some php code written by Josh. Will need some front end work though and additon functions

--- database/dbAddVolunteer.php
+++ database/dbAddVolunteer.php
@@ -3,8 +3,8 @@
 include_once('dbinfo.php');
 include_once(dirname(__FILE__).'/../domain/Volunteer.php');
 
-//add a volunteer to dbvolunteer
 
+// just a random function doesnt do anything
 function add_volunteer($volunteer) {
     if (!$volunteer instanceof Volunteer)
         die("Error: add_volunteer type mismatch");
@@ -13,10 +13,10 @@ function add_volunteer($volunteer) {
     $result = mysqli_query($con,$query);
     //if there's no entry for this id, add it
     if ($result == null || mysqli_num_rows($result) == 0) {
-        mysqli_query($con,'INSERT INTO dbPersons VALUES("' .
-            $person->get_id() . '","' .
-            $person->get_start_date() . '","' .
-            $person->get_venue() . '","' .
+        mysqli_query($con,'INSERT INTO dbvolunteer VALUES("' .
+            $volunteer->getFirstName() . '","' .
+            $volunteer->getLastName() . '","' .
+            $volunteer->getEmail() . '","' .
             '");'
 
         );							
@@ -25,4 +25,27 @@ function add_volunteer($volunteer) {
     }
     mysqli_close($con);
     return false;
+}
+
+function create_volunteer($volunteer){
+    $con =connect();
+    $firstname = $volunteer['first-name'];
+    $lastname = $volunteer['last-name'];
+    $email = $volunteer['email'];
+    $query = "INSERT INTO dbvolunteer (firstName, lastName, email) VALUES('$firstname', '$lastname', '$email')";
+    try {
+        $result = mysqli_query($con, $query);
+        mysqli_commit($con);
+        mysqli_close($con);
+        return $email;
+    } catch (mysqli_sql_exception $e) {
+        // Check if the error is due to duplicate entry for vendorName
+        if ($e->getCode() === 1062) { // Error code for duplicate entry
+            return null; // or handle the error in some other way
+        } else {
+            throw $e; // Re-throw other exceptions
+        }
+    }
+
+
 }
