e68ad8983922458a4e7bee112fb3dd79b131fb53
gcarlton <gcarlton@mail.umw.edu>
2024-04-15 21:46:12 -0400

    current families report works and provides a CSV

--- header.php
+++ header.php
@@ -104,6 +104,7 @@
         $permission_array['approve.php'] = 2;
         $permission_array['viewadmin.php'] = 2;
         $permission_array['modifyfamily.php'] = 2;
+        $permission_array['currentFamiliesReport.php'] = 2;
         //need to update permission later
         $permission_array['modifyadminform.php'] = 0;
       
