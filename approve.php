20e7c7e259ce80b9a788b2df00f139c709617d96
aseidens <aseidens@mail.umw.edu>
2024-04-15 21:00:16 -0400

    changes for siteground functionality in approve.php
    removed update_status function definition from approve.php (it's in dbPersons.php). updated connection in this file to use connect() function which checks for siteground db

--- approve.php
+++ approve.php
@@ -4,9 +4,9 @@
 session_start();
 
 require_once('header.php');
-
+require_once('database/dbPersons.php');
 // Connect to the database
-$hostname = "localhost"; 
+/*$hostname = "localhost"; 
 $database = "fgp";
 $username = "fgp";
 $password = "fgp";
@@ -16,17 +16,18 @@ $connection = mysqli_connect($hostname, $username, $password, $database);
 // Check if the connection was successful
 if (!$connection) {
     die("Connection failed: " . mysqli_connect_error());
-}
+}*/
 
 // Function to update status
-function update_status($id, $new_status) {
-    global $connection; // Access the global $connection variable
+/*function update_status($id, $new_status) {
+    $con=connect();
+    //global $connection; // Access the global $connection variable
     $query = "UPDATE dbPersons SET status = '$new_status' WHERE id = '$id'";
     $result = mysqli_query($connection, $query);
     if (!$result) {
         die("Update failed: " . mysqli_error($connection));
     }
-}
+}*/
 
 // Check if the "Approve" or "Reject" button is clicked
 if (isset($_POST['approve'])) {
@@ -39,8 +40,9 @@ if (isset($_POST['approve'])) {
     update_status($id, 'Rejected'); // Set status to 'Rejected'
 }
 // Query to fetch all families from the database
+$con=connect();
 $query = "SELECT * FROM dbPersons WHERE type = 'family' AND status = 'pending' ORDER BY last_name";
-$result = mysqli_query($connection, $query);
+$result = mysqli_query($con, $query);
 
 if (!$result) {
     die("Database query failed.");
@@ -89,6 +91,6 @@ while ($row = mysqli_fetch_assoc($result)) {
 
 <?php
 // Close the database connection
-mysqli_close($connection);
+//mysqli_close($connection);
 require_once('universal.inc');
 ?>
