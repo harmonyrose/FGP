a4964309e256504d995e8429db901d6103b15a7b
hpeura <hpeura@mail.umw.edu>
2024-05-01 11:59:47 -0400

    Fixed header issue with points program form

--- pointsProgForm.php
+++ pointsProgForm.php
@@ -5,22 +5,22 @@
     ini_set("display_errors",1);
     error_reporting(E_ALL);
 
-    $loggedIn = false;
-    $accessLevel = 0;
-    $userID = null;
-    if (isset($_SESSION['_id'])) {
-        $loggedIn = true;
-        // 0 = not logged in, 1 = standard user, 2 = manager (Admin), 3 super admin (TBI)
-        $accessLevel = $_SESSION['access_level'];
-        $userID = $_SESSION['_id'];
-    } 
+    // $loggedIn = false;
+    // $accessLevel = 0;
+    // $userID = null;
+    // if (isset($_SESSION['_id'])) {
+    //     $loggedIn = true;
+    //     // 0 = not logged in, 1 = standard user, 2 = manager (Admin), 3 super admin (TBI)
+    //     $accessLevel = $_SESSION['access_level'];
+    //     $userID = $_SESSION['_id'];
+    // } 
 
     // Do not require admin perms
-    if ($accessLevel < 1) {
-        header('Location: login.php');
-        echo 'bad access level';
-        die();
-    }
+    // if ($accessLevel < 1) {
+    //     header('Location: login.php');
+    //     echo 'bad access level';
+    //     die();
+    // }
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         require_once('include/input-validation.php');
         require_once('database/dbPointsProg.php');
